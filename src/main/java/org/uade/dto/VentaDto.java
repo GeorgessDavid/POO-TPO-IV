b882b5dd50efd2e6a9ed08c20a3a08e2d81ab56f
Frank441 <stormform1234@gmail.com>
2025-06-27 18:44:51 -0300

    refactor(dtos): change Venta dto name convention

--- src/main/java/org/uade/dto/VentaDto.java
+++ src/main/java/org/uade/dto/VentaDto.java
@@ -1,10 +1,45 @@
 package org.uade.dto;
 
-public class VentaDto {
+import org.uade.model.Combo;
+import org.uade.model.Funcion;
+import org.uade.model.TarjetaDescuento;
 
-    public VentaDto(FuncionDTO funcion) {
+import java.util.Date;
+import java.util.List;
 
+public class VentaDTO {
+    private final int ventaID;
+    private final Date fchVenta;
+    private final List<Combo> combos;
+    private final Funcion funcion;
+    private final TarjetaDescuento tarjetaDescuento;
+
+    public VentaDTO(int ventaID, Date fchVenta, List<Combo> combos, Funcion funcion, TarjetaDescuento tarjetaDescuento) {
+        this.ventaID = ventaID;
+        this.fchVenta = fchVenta;
+        this.combos = combos;
+        this.funcion = funcion;
+        this.tarjetaDescuento = tarjetaDescuento;
     }
 
+    public int getVentaID() {
+        return ventaID;
+    }
+
+    public Date getFchVenta() {
+        return fchVenta;
+    }
+
+    public List<Combo> getCombos() {
+        return combos;
+    }
+
+    public Funcion getFuncion() {
+        return funcion;
+    }
+
+    public TarjetaDescuento getTarjetaDescuento() {
+        return tarjetaDescuento;
+    }
 
 }
